4b1d97cc2e3929f86333fd6765fe1dd0fd9e72d6
Christopher <christopherjavier1998@gmail.com>
2023-11-15 09:20:08 +0100

    prueba

---
+++ Curso-JAVA-master/src/EJERCICIOS/EJERCICIO5.java
@@ -0,0 +1,39 @@
+package EJERCICIOS;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class EJERCICIO5 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingresa un numero:");
+        int numero = entrada.nextInt();
+
+        int numDigitos = contarDigitos(numero);
+
+        int suma = sumarDigitosElevados(numero, numDigitos);
+
+        if (suma == numero) {
+            System.out.println(numero + " es un numero Amstrong.");
+        } else {
+            System.out.println(numero + " no es un numero Amstrong.");
+        }
+    }
+
+    private static int contarDigitos(int num) {
+        return(int) Math.floor(Math.log10(num) + 1);
+    }
+
+    private static int sumarDigitosElevados(int num, int potencia) {
+        int suma = 0;
+        int originalNum = num;
+
+        while (num != 0) {
+            int digito = num % 10;
+            suma += Math.pow(digito, potencia);
+            num /= 10;
+        }
+
+        return suma;
+    }
+}
---
+++ Curso-JAVA-master/src/EJERCICIOS/EJERCICIO6.java
@@ -0,0 +1,26 @@
+package EJERCICIOS;
+import java.util.Scanner;
+public class EJERCICIO6 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Metros\tPulgadas");
+        System.out.println("----------------");
+
+        for(int pulgadas = 1; pulgadas <= 144; pulgadas++){
+
+            double metros = convertirPulgadasAMetros(pulgadas);
+            System.out.printf("%.2f\t%d\n", metros, pulgadas);
+
+            if (pulgadas % 12 == 0) {
+                System.out.println();
+            }
+        }
+    }
+
+    private static double convertirPulgadasAMetros(int pulgadas) {
+        return pulgadas * 0.0254;
+
+    }
+}
